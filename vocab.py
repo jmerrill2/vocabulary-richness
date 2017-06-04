b2c239234f8d3e0cffe6f764a7aeaff56ebe9a0a
jared <jmerrill@izeni.com>
2017-06-03 22:35:33 -0600

    Included link to source.

--- vocab.py
+++ vocab.py
@@ -1,3 +1,5 @@
+# https://swizec.com/blog/measuring-vocabulary-richness-with-python/swizec/2528
+
 from nltk.stem.porter import PorterStemmer
 from itertools import groupby
 import sys
@@ -20,6 +22,13 @@ def yule(entry):
             d[w] = 1
 
     M1 = float(len(d))
+
+    # d.values() is the number of instances of each word in the text
+    # groupby groups them by similar instance count. freq is that count, len(list(g)) is the number
+    # of times words of that value count appeared. For example, if "foo" and "bar" were each in the
+    # text 3 times, and no other words in the text appeared 3 times, freq for that instance would be 3,
+    # and len(list(g)) would be 2 - in other words there were exactly two words in the text that 
+    # appeared three times. 
     M2 = sum([len(list(g)) * (freq ** 2) for freq, g in
               groupby(sorted(d.values()))])
 
@@ -37,4 +46,5 @@ if __name__ == '__main__':
     textfile = sys.argv[1]
     with open(textfile, 'r') as i:
         lines = i.read().replace('\n', '')
+    print('number of words: ', len(lines))
     yule(lines)
